ecd850b277bce9235651f688ac78a17211a21322
Qunda <qmateza@gmail.com>
2025-12-09 12:52:34 +0200

    Fixed routing

--- base/views.py
+++ base/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(requests):
   return render(requests, 'home.html')
+
+def about(requests):
+  return render(requests, 'about.html')
+
+def products(requests):
+  return render(requests, 'products.html')
+
+def contact(requests):
+  return render(requests, 'contact.html')
